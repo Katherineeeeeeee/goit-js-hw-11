92e5ccae5b4d202fcca59a80ea944e0ceff8a052
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-07-21 18:42:23 +0300

    added new function

--- src/js/api.js
+++ src/js/api.js
@@ -8,21 +8,26 @@ export class Api {
 
   constructor() {
     this.page = 1;
+    this.totalHits = null;
     this.query = null;
+    this.per_page = 40;
   }
 
   fetchPhotosByQuery() {
     const searchParams = new URLSearchParams({
-    page: this.page,
-    per_page: 10, 
+      page: this.page,
+      per_page: this.per_page,
 
-    key: this.#API_KEY,
-    q: this.query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true
-    })
-
+      key: this.#API_KEY,
+      q: this.query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    });
     return axios.get(`${this.#URL}/?${searchParams}`);
-}
+  }
+
+  isNextDataExist() {
+    return this.page * this.per_page < this.totalHits;
+  }
 }
